1471df290d9b34f2da93606b5231c81585f38cb0
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-08 10:48:02 +0200

    Add test_prime_power_number_prime_factors

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -10,12 +10,22 @@ def test_prime_number_prime_factors():
     result = prime_factors(prime_number)
     assert result == [prime_number], f'Expected [{prime_number}], got {result}'
 
+def test_prime_power_number_prime_factors():
+    prime_number = 3
+    power = 4
+    test_number = pow(prime_number, power)
+    expected = [prime_number for x in range(power)]
+    result = prime_factors(test_number)
+    assert expected == result, f'Expected {expected}, got {result}'
+
+
 from prime_factors import prime_factors
 
 if __name__ == '__main__':
     for test in (
         test_import_prime_factors,
-        test_prime_number_prime_factors
+        test_prime_number_prime_factors,
+        test_prime_power_number_prime_factors
     ):
         print(f'{test.__name__}: ', end='')
         try:
