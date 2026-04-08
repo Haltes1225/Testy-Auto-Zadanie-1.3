e4b277cec5174091743145e718d3fd0658ad7814
Haltes1225 <wiktor.jochymczyk@gmail.com>
2026-04-08 11:30:48 +0200

    Add test_semiprime_number_prime_factors

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,3 +1,5 @@
+from sympy import *
+
 def test_import_prime_factors():
     try:
         from prime_factors import prime_factors
@@ -18,6 +20,15 @@ def test_prime_power_number_prime_factors():
     result = prime_factors(test_number)
     assert expected == result, f'Expected {expected}, got {result}'
 
+def test_semiprime_number_prime_factors():
+    prime_number_1 = 3
+    prime_number_2 = 17
+    test_number = floor(prime_number_1 * prime_number_2)
+    expected = [prime_number_1, prime_number_2]
+    result = prime_factors(test_number)
+    print(result)
+    assert expected == result, f'Expected {expected}, got {result}'
+
 
 from prime_factors import prime_factors
 
@@ -25,7 +36,8 @@ if __name__ == '__main__':
     for test in (
         test_import_prime_factors,
         test_prime_number_prime_factors,
-        test_prime_power_number_prime_factors
+        test_prime_power_number_prime_factors,
+        test_semiprime_number_prime_factors
     ):
         print(f'{test.__name__}: ', end='')
         try:
